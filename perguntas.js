76d5d6ad9bec9f0f759af1341c22431b5cbdc0c4
jrb-1979 <julia.buzetti@escola.pr.gov.br>
2025-05-31 03:00:25 +0000

    aula 8

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,45 @@ criaCartao(
     'Qual o nome do rio que separa o reino dos vivos e dos mortos na Mitologia Grega?',
     'É Rio Estige.'
 )
+
+criaCartao(
+    'Mitologia Nórdica',
+    'O que é o Ragnarök?',
+    'Na mitologia nórdica, Ragnarök é uma série de eventos que levará ao fim do mundo e na qual haverá uma grande batalha final.'
+)
+
+criaCartao(
+    'Mitologia Nórdica',
+    'Quantos mundos exitem na Mitologia Nórdica?',
+    'Na mitologia nórdica, existem nove mundos, todos conectados pela árvore gigante Yggdrasil, são eles Asgard, Midgard, Vanaheimr, Jötunheimr, Niflheim, Muspelheim, Alfheim, Niðavellir e Helheim.'
+)
+
+criaCartao(
+    'Mitologia Nórdica',
+    'Qual deus nórdico se vestiu de noiva?',
+    'Thor, que para recuperar seu martelo roubado entrou em uma missão, na qual teve que se vestir de noiva.'
+)
+
+criaCartao(
+    'Mitologia Egípcia',
+    'Qual o nome da deusa egípcia que era metade gato?',
+    'Seu nome é Bastet, é a deusa da casa, da fertilidade, da música, da proteção e dos gatos'
+)
+
+criaCartao(
+    'Mitologia Egípcia',
+    'Quem era Anúbis?',
+    'Anúbis é o deus da morte e da mumificação'
+)
+
+criaCartao(
+    'Mitologia Egípcia',
+    'Qual o nome do deus egípcio que ressussitou?',
+    'Osirís, ele foi morto e esquartejado pelo irmão Seth, que desejava tomar o trono, porém ressussitou e passou a governar o pós-vida'
+)
+
+criaCartao(
+    'Mitologia Egípcia',
+    'Que objetos eram utilizados para julgar o morto no pós-vida?',
+    'O coração do morto e a Pena da Verdade, que eram colocados em uma balança, caso o coração fosse mais pesado que a pena, o falecido seria devorado, caso contrário, alcançaria a vida eterna.'
+)
