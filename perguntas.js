d166b3719d8acafe4437dfaecbabee7996f05dc9
jrb-1979 <julia.buzetti@escola.pr.gov.br>
2025-05-31 01:51:12 +0000

    aula 6

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Mitologia Grega',
+    'Quem é o deus grego da Morte?',
+    'O deus grego da morte e governante submundo é Hades.'
+)
+
+criaCartao (
+    'Mitologia Grega',
+    'De onde nasceu a deusa grega Atena?',
+    'A deusa da sabedoria e da guerra Atena nasceu da cabeça de seu pai, Zeus, que após engolir a mãe de Atena, gerou a deusa em sua mente.'
+)
+
+criaCartao (
+    'Mitologia Grega',
+    'Qual o nome do rio que separa o reino dos vivos e dos mortos na Mitologia Grega?',
+    'É Rio Estige.'
+)
